f35da8a3f1b4e1129e8900975a4363cccacea425
colin <287268221@qq.com>
2019-09-10 11:27:19 +0800

    update

---
+++ lib/Routes.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'locale/translations_delegate.dart';
+
+class Routes extends StatelessWidget {
+  List<ListItem> listData;
+
+  void initData(BuildContext context) {
+    listData = [];
+    listData.add(ListItem(
+        Translations.of(context).text('homePage'), "Home", Icons.home));
+    listData.add(ListItem("Pavlova示例", "Pavlova", Icons.panorama_vertical));
+    listData.add(ListItem("randomWords示例", "randomWords", Icons.book));
+    listData.add(ListItem("图片", "Image", Icons.image));
+    listData.add(ListItem("GridView", "GridView", Icons.grid_on));
+    listData.add(ListItem("StateWidget", "StateWidget", Icons.widgets));
+    listData.add(ListItem("TextField", "TextField", Icons.text_fields));
+    listData.add(ListItem("KeepAlive", "KeepAlive", Icons.live_tv));
+    listData.add(ListItem("Swiper", "Swiper", Icons.landscape));
+    listData.add(ListItem("流式布局", "Wrap", Icons.landscape));
+    listData.add(ListItem("动画", "Animation", Icons.landscape));
+    listData.add(ListItem('动画2', 'AnimationTwo', Icons.android));
+    listData.add(ListItem("屏幕适配示例", "ScreenUtilPage", Icons.landscape));
+    listData.add(
+        ListItem("futureBuilder", "FutureBuilderPage", Icons.hourglass_full));
+    listData.add(ListItem("主题", "ThemePage", Icons.landscape));
+    listData.add(ListItem("Chip", "ChipPage", Icons.landscape));
+    listData
+        .add(ListItem("ExpansionTile", "ExpansionTilePage", Icons.landscape));
+    listData.add(ListItem("Transform 3D效果", "TransformPage", Icons.landscape));
+    listData.add(ListItem("登录页", "LoginPage", Icons.landscape));
+    listData.add(ListItem("WebView", "WebViewPage", Icons.web));
+    listData.add(ListItem("自定义View", "CustomViewPage", Icons.view_quilt));
+    listData.add(ListItem("Tab的使用", "TabPae", Icons.view_quilt));
+    listData.add(ListItem("backdropPage", "backdropPage", Icons.view_quilt));
+    listData.add(ListItem('可拖动组件', 'Draggable', Icons.drag_handle));
+    listData.add(ListItem('分享', 'SharePage', Icons.share));
+    listData.add(ListItem('搜索', 'SearchPage', Icons.search));
+    listData.add(ListItem('mp4视频播放', 'VideoPlayerPage', Icons.video_library));
+    listData.add(
+        ListItem('Youtube视频播放', 'FlutterYoutubePage', Icons.video_library));
+    listData.add(
+        ListItem('Youtube视频播放2', 'YoutubePlayerPage', Icons.video_library));
+    listData
+        .add(ListItem('FlutterNativeWeb插件的使用', 'FlutterNativeWeb', Icons.web));
+    listData.add(ListItem('sqflite的使用', 'SqfLitePage', Icons.crop_square));
+    listData.add(ListItem('步骤条Stepper ', 'StepperPage', Icons.short_text));
+    listData.add(ListItem('类似于qq列表的组件树', 'TreePage', Icons.people));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    initData(context);
+
+    //设置适配尺寸 (填入设计稿中设备的屏幕尺寸) 假如设计稿是按iPhone6的尺寸设计的(iPhone6 750*1334)
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('列表'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return ListItemWidget(listData[index]);
+        },
+        itemCount: listData.length,
+      ),
+    );
+  }
+}
+
+class ListItem {
+  final String title;
+  final String routeName;
+  final IconData iconData;
+
+  ListItem(this.title, this.routeName, this.iconData);
+}
+
+class ListItemWidget extends StatelessWidget {
+  final ListItem listItem;
+
+  ListItemWidget(this.listItem);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text(listItem.title),
+        leading: Icon(listItem.iconData),
+        trailing: Icon(Icons.arrow_forward),
+        onTap: () {
+          Navigator.pushNamed(context, listItem.routeName);
+        });
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,277 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+import 'dart:ui' as ui;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: _formKey,
+        body: Center(
+          child: GridView.count(
+            //横轴子元素的数量。此属性值确定后子元素在横轴的长度就确定了，即ViewPort横轴长度除以crossAxisCount的商。
+            crossAxisCount: 2,
+            //子元素在横轴长度和主轴长度的比例。由于crossAxisCount指定后，子元素横轴长度就确定了，然后通过此参数值就可以确定子元素在主轴的长度
+            childAspectRatio: 0.8,
+            //主轴方向的间距。
+            mainAxisSpacing: 10,
+            //横轴方向子元素的间距。
+            crossAxisSpacing: 10,
+            children: <Widget>[
+              BatteryStatusCardWidget(),
+              TodayUsedCardWidget(),
+              NetworkStatusCardWidget(),
+              ConnectCardWidget(),
+            ],
+          ),
+        ));
+  }
+}
+
+class BatteryStatusCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0x40ffffff),
+      //设置shape，这里设置成了R角
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(0.0)),
+      ),
+      //对Widget截取的行为，比如这里 Clip.antiAlias 指抗锯齿
+      clipBehavior: Clip.antiAlias,
+      semanticContainer: false,
+      child: getChild(),
+    );
+  }
+
+  getChild() {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: CustomPaint(painter: BatteryViewPainter(30)),
+    );
+  }
+}
+
+class TodayUsedCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0x40ffffff),
+      //设置shape，这里设置成了R角
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(0.0)),
+      ),
+      //对Widget截取的行为，比如这里 Clip.antiAlias 指抗锯齿
+      clipBehavior: Clip.antiAlias,
+      semanticContainer: false,
+      child: getChild(),
+    );
+  }
+
+  getChild() {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: CustomPaint(
+          child: Center(child: Text((30).round().toString())),
+          painter: CircleProgressBarPainter(30)),
+    );
+  }
+}
+
+class NetworkStatusCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0x40ffffff),
+      //设置shape，这里设置成了R角
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(0.0)),
+      ),
+      //对Widget截取的行为，比如这里 Clip.antiAlias 指抗锯齿
+      clipBehavior: Clip.antiAlias,
+      semanticContainer: false,
+      child: getChild(),
+    );
+  }
+
+  getChild() {
+    return Container(
+      width: 200,
+      height: 150,
+      alignment: Alignment.center,
+      child: ListView(
+        padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+        children: <Widget>[
+          Image(
+            width: 100,
+            height: 76,
+            fit: BoxFit.contain,
+            image: AssetImage("assets/images/signal_0.png"),
+          ),
+          Text("网络状态:",
+              style: TextStyle(color: Colors.black, fontSize: 25.0),
+              textAlign: TextAlign.center)
+        ],
+      ),
+    );
+  }
+}
+
+class ConnectCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0x40ffffff),
+      //设置shape，这里设置成了R角
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(0.0)),
+      ),
+      //对Widget截取的行为，比如这里 Clip.antiAlias 指抗锯齿
+      clipBehavior: Clip.antiAlias,
+      semanticContainer: false,
+      child: getChild(),
+    );
+  }
+
+  getChild() {
+    return Container(
+      width: 200,
+      height: 150,
+      alignment: Alignment.center,
+      child: ListView(
+        padding: EdgeInsets.all(20),
+        children: <Widget>[
+          Image(
+            width: 100,
+            height: 100,
+            image: AssetImage("assets/images/connection_icon.png"),
+          ),
+          Text("连接设备:",
+              style: TextStyle(color: Colors.black, fontSize: 25.0),
+              textAlign: TextAlign.center)
+        ],
+      ),
+    );
+  }
+}
+
+class CircleProgressBarPainter extends CustomPainter {
+  Paint _paintBackground;
+  Paint _paintFore;
+  final double pi = 3.1415926;
+  var progress;
+
+  CircleProgressBarPainter(this.progress) {
+    _paintBackground = Paint()
+      ..color = Colors.grey
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10.0
+      ..isAntiAlias = true;
+    _paintFore = Paint()
+      ..color = Colors.red
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10.0
+      ..isAntiAlias = true;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), size.width / 2,
+        _paintBackground);
+    Rect rect = Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: size.width / 2,
+    );
+    //canvas.drawArc(rect, 0.0, progress * 3.14 / 180, false, _paintFore);
+    canvas.drawArc(rect, 0.0, 2 * pi * progress / 100, false, _paintFore);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class BatteryViewPainter extends CustomPainter {
+  Paint _paintBackground;
+  Paint _paintFore;
+
+  int power = 100;
+  int mColor;
+  bool charge;
+
+  BatteryViewPainter(this.power) {
+    _paintBackground = Paint()
+      ..color = Colors.grey
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.0
+      ..isAntiAlias = true;
+    _paintFore = Paint()
+      ..color = Colors.red
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.0
+      ..isAntiAlias = true;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double width = size.width;
+    double height = size.height;
+
+    var headHeight = size.height / 20.0;
+
+    Rect headRect = new Rect.fromPoints(
+        Offset(width / 3.0, 0), Offset(width * 0.75, headHeight));
+    canvas.drawRect(headRect, _paintBackground);
+
+    double topOffset = (height - headHeight) * (100 - power) / 100.0;
+    Rect rect = new Rect.fromPoints(
+        Offset(0, headHeight), Offset(width, headHeight + topOffset));
+
+    canvas.drawRect(rect, _paintBackground);
+
+    Rect rect2 = new Rect.fromPoints(
+        Offset(0, headHeight + topOffset), Offset(width, height));
+    canvas.drawRect(rect2, _paintFore);
+
+    //if (charge) {
+    var sunImage = AssetImage("assets/images/charger.png");
+
+    sunImage.obtainKey(ImageConfiguration()).then((AssetBundleImageKey key) {
+      _loadImage(key).then((ui.Codec codec) {
+        print("frameCount: ${codec.frameCount.toString()}");
+        codec.getNextFrame().then((info) {
+          print("image: ${info.image.toString()}");
+          print("duration: ${info.duration.toString()}");
+          canvas.drawImage(info.image, Offset(20, 20), Paint());
+        });
+      });
+    });
+    //}
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+
+  Future<ui.Codec> _loadImage(AssetBundleImageKey key) async {
+    final ByteData data = await key.bundle.load(key.name);
+    if (data == null) throw 'Unable to read data';
+    return await ui.instantiateImageCodec(data.buffer.asUint8List());
+  }
+}
