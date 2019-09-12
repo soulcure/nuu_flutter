0617ddff6b4279983bcff925e7afd979e1986bca
colin <287268221@qq.com>
2019-09-12 09:34:17 +0800

    update

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -4,6 +4,10 @@
  *
  */
 import 'package:flutter/material.dart';
+import 'package:async/async.dart';
+import 'package:konnect/utils/HttpUtil.dart';
+
+import 'config/AppConfig.dart';
 
 class TabPage extends StatefulWidget {
   @override
@@ -56,21 +60,74 @@ class _TabPageState extends State<TabPage> with SingleTickerProviderStateMixin {
 }
 
 class TarItemState extends State<TabItemView> {
+  AsyncMemoizer _memoizer = AsyncMemoizer();
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        alignment: Alignment.center,
-        child: Text(
-          widget.item.toString(),
-          style: TextStyle(color: Colors.white),
-        ),
-        decoration: BoxDecoration(
-          color: widget.item == 2 ? Colors.blue : Colors.green,
+    return Scaffold(
+      body: RefreshIndicator(
+        onRefresh: _refreshData,
+        child: FutureBuilder(
+          builder: _buildFuture,
+          future: _gerData(), // 用户定义的需要异步执行的代码，类型为Future<String>或者null的变量或函数
         ),
       ),
     );
+  }
+
+  _gerData() {
+    return _memoizer.runOnce(() async {
+      return await HttpUtil().get(AppConfig.NEWS);
+    });
+  }
+
+  Future _refreshData() async {
+    setState(() {
+      _memoizer = AsyncMemoizer();
+    });
+  }
+
+  ///snapshot就是_calculation在时间轴上执行过程的状态快照
+  Widget _buildFuture(BuildContext context, AsyncSnapshot snapshot) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.none:
+        print('还没有开始网络请求');
+        return Text('还没有开始网络请求');
+      case ConnectionState.active:
+        print('active');
+        return Text('ConnectionState.active');
+      case ConnectionState.waiting:
+        print('waiting');
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      case ConnectionState.done:
+        print('done');
+        if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+        return _createListView(context, snapshot);
+      default:
+        return Text('还没有开始网络请求');
+    }
+  }
+
+  Widget _createListView(BuildContext context, AsyncSnapshot snapshot) {
+    List movies = snapshot.data['data'];
+    return ListView.builder(
+      itemBuilder: (context, index) => _itemBuilder(context, index, movies),
+      itemCount: movies.length * 2,
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, int index, skills) {
+    if (index.isOdd) {
+      return Divider();
+    }
+    index = index ~/ 2;
+    return Column(children: <Widget>[
+      Text(skills[index]['title']),
+      Text(skills[index]['Content']),
+      Text(skills[index]['Time']),
+    ]);
   }
 
   @override
