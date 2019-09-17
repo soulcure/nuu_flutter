68ba4c0de5126f5896e1c6ec0260be94af49c512
colin <287268221@qq.com>
2019-09-17 13:43:46 +0800

    update

--- lib/home/HomeFragment.dart
+++ lib/home/HomeFragment.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show ByteData, rootBundle;
+import 'package:flutter_statusbar/flutter_statusbar.dart';
 import 'dart:ui' as ui;
 
 import 'BatteryViewPainter.dart';
@@ -15,29 +16,54 @@ class HomeFragment extends StatefulWidget {
 
 class _HomePageState extends State<HomeFragment> {
   final _formKey = GlobalKey<FormState>();
+  double _statusBarHeight = 84;
+
+  getData() async {
+    double height = await FlutterStatusbar.height;
+    setState(() => _statusBarHeight = height);
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        key: _formKey,
-        body: Center(
-          child: GridView.count(
-            //横轴子元素的数量。此属性值确定后子元素在横轴的长度就确定了，即ViewPort横轴长度除以crossAxisCount的商。
-            crossAxisCount: 2,
-            //子元素在横轴长度和主轴长度的比例。由于crossAxisCount指定后，子元素横轴长度就确定了，然后通过此参数值就可以确定子元素在主轴的长度
-            childAspectRatio: 0.8,
-            //主轴方向的间距。
-            mainAxisSpacing: 10,
-            //横轴方向子元素的间距。
-            crossAxisSpacing: 10,
-            children: <Widget>[
-              BatteryStatusCardWidget(),
-              TodayUsedCardWidget(),
-              NetworkStatusCardWidget(),
-              ConnectCardWidget(),
-            ],
-          ),
-        ));
+    final screenW = MediaQuery.of(context).size.width; //屏幕宽
+    final screenH = MediaQuery.of(context).size.height; //屏幕高
+
+    var widgetW = screenW;
+    var widgetH = screenH - _statusBarHeight;
+
+    const int padding = 5;
+    const int spacing = 10;
+
+    var w = (widgetW - 2 * padding - spacing) / 2;
+    var h = (widgetH - 2 * padding - spacing) / 2;
+
+    var ratio = w / h;
+
+    return Container(
+      key: _formKey,
+      child: GridView.count(
+        //横轴子元素的数量。此属性值确定后子元素在横轴的长度就确定了，即ViewPort横轴长度除以crossAxisCount的商。
+        crossAxisCount: 2,
+        //子元素在横轴长度和主轴长度的比例。由于crossAxisCount指定后，子元素横轴长度就确定了，然后通过此参数值就可以确定子元素在主轴的长度
+        childAspectRatio: ratio,
+        //主轴方向的间距。
+        mainAxisSpacing: 10,
+        //横轴方向子元素的间距。
+        crossAxisSpacing: 10,
+        children: <Widget>[
+          BatteryStatusCardWidget(),
+          TodayUsedCardWidget(),
+          NetworkStatusCardWidget(),
+          ConnectCardWidget(),
+        ],
+      ),
+    );
   }
 }
 
