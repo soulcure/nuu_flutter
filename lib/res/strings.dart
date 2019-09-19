68096f5e6b39f1e9c08381b5c8cae318e9df4149
colin <287268221@qq.com>
2019-09-19 17:16:51 +0800

    update

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -9,6 +9,20 @@ class Ids {
   static const String menuContact = 'menu_contact';
   static const String menuLogout = 'menu_logout';
 
+  //login
+  static const String promptEmail = 'prompt_email';
+  static const String promptPassword = 'prompt_password';
+  static const String actionSignIn = 'action_sign_in';
+  static const String actionSignInShort = 'action_sign_in_short';
+  static const String actionRegister = 'action_register';
+  static const String lostPassword = 'lost_password';
+  static const String lostEmail = 'lost_email';
+  static const String logoutMsg = 'logout_msg';
+  static const String welcome = 'welcome';
+  static const String invalidUsername = 'invalid_username';
+  static const String invalidPassword = 'invalid_password';
+  static const String loginFailed = 'login_failed';
+
   static const String confirm = 'confirm';
   static const String cancel = 'cancel';
 
@@ -27,9 +41,24 @@ Map<String, Map<String, Map<String, String>>> localizedValues = {
       Ids.menuLogin: 'Login',
       Ids.menuContact: 'Contact',
       Ids.menuLogout: 'Logout',
+
       Ids.confirm: 'Confirm',
       Ids.cancel: 'Cancel',
       Ids.connectCount: 'Connecting device:%\$0\$s',
+
+      //login
+      Ids.promptEmail: 'prompt_email',
+      Ids.promptPassword: 'prompt_password',
+      Ids.actionSignIn: 'action_sign_in',
+      Ids.actionSignInShort: 'action_sign_in_short',
+      Ids.actionRegister: 'action_register',
+      Ids.lostPassword: 'lost_password',
+      Ids.lostEmail: 'lost_email',
+      Ids.logoutMsg: 'logout_msg',
+      Ids.welcome: 'welcome',
+      Ids.invalidUsername: 'invalid_username',
+      Ids.invalidPassword: 'invalid_password',
+      Ids.loginFailed: 'login_failed',
     }
   },
   'zh': {
@@ -46,6 +75,20 @@ Map<String, Map<String, Map<String, String>>> localizedValues = {
       Ids.confirm: '确认',
       Ids.cancel: '取消',
       Ids.connectCount: '连接设备：%\$0\$s',
+
+      //login
+      Ids.promptEmail: '电子邮件地址或用户名',
+      Ids.promptPassword: '密码',
+      Ids.actionSignIn: '登录',
+      Ids.actionSignInShort: '登录',
+      Ids.actionRegister: '注册',
+      Ids.lostPassword: '忘了你的密码?',
+      Ids.lostEmail: '忘了你的电子邮箱?',
+      Ids.logoutMsg: '退出您的帐户?',
+      Ids.welcome: '欢迎 !',
+      Ids.invalidUsername: '不是有效的用户名',
+      Ids.invalidPassword: '密码必须多于5个字符',
+      Ids.loginFailed: '登录失败',
     },
     'HK': {
       Ids.menuHome: '主頁',
@@ -60,6 +103,20 @@ Map<String, Map<String, Map<String, String>>> localizedValues = {
       Ids.confirm: '确认',
       Ids.cancel: '取消',
       Ids.connectCount: '連接設備:%\$0\$s',
+
+      //login
+      Ids.promptEmail: '電子郵件地址或用戶名',
+      Ids.promptPassword: '密碼',
+      Ids.actionSignIn: '登錄',
+      Ids.actionSignInShort: '登錄',
+      Ids.actionRegister: '註冊',
+      Ids.lostPassword: '忘了你的密碼?',
+      Ids.lostEmail: '忘了你的電子郵箱?',
+      Ids.logoutMsg: '退出您的帳戶?',
+      Ids.welcome: '歡迎 !',
+      Ids.invalidUsername: '不是有效的用戶名',
+      Ids.invalidPassword: '密碼必須多於5個字符',
+      Ids.loginFailed: '登錄失敗',
     },
     'TW': {
       Ids.menuHome: '主頁',
@@ -74,6 +131,20 @@ Map<String, Map<String, Map<String, String>>> localizedValues = {
       Ids.confirm: '确认',
       Ids.cancel: '取消',
       Ids.connectCount: '連接設備:%\$0\$s',
+
+      //login
+      Ids.promptEmail: '電子郵件地址或用戶名',
+      Ids.promptPassword: '密碼',
+      Ids.actionSignIn: '登錄',
+      Ids.actionSignInShort: '登錄',
+      Ids.actionRegister: '註冊',
+      Ids.lostPassword: '忘了你的密碼?',
+      Ids.lostEmail: '忘了你的電子郵箱?',
+      Ids.logoutMsg: '退出您的帳戶?',
+      Ids.welcome: '歡迎 !',
+      Ids.invalidUsername: '不是有效的用戶名',
+      Ids.invalidPassword: '密碼必須多於5個字符',
+      Ids.loginFailed: '登錄失敗',
     }
   }
 };
