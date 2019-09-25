b5ceba5d9ae09502882ebb4fa3beb0b98a3c21e0
colin <287268221@qq.com>
2019-09-25 15:18:34 +0800

    update

--- lib/DevicesFragment.dart
+++ lib/DevicesFragment.dart
@@ -15,16 +15,19 @@ class _FutureBuilderState extends State<DevicesFragment> {
     var db = DatabaseHelper();
     var res = await db.getAllDevices();
     print('Devices:$res');
-    setState(() {
-      List listData = [];
-      listData.addAll(res);
-      List<Device> list = [];
-      if (listData.length > 0) {
-        for (var item in listData) {
-          Device device = Device.fromMap(item);
-          list.add(device);
-        }
+
+    List listData = [];
+    listData.addAll(res);
+
+    List<Device> list = [];
+    if (listData.length > 0) {
+      for (var item in listData) {
+        Device device = Device.fromMap(item);
+        list.add(device);
       }
+    }
+
+    setState(() {
       _devicesKey.currentState.onSuccess(list);
     });
   }
@@ -42,7 +45,7 @@ class _FutureBuilderState extends State<DevicesFragment> {
             child: AppBar(
               title: Text("连接的设备",
                   style: TextStyle(fontSize: 18.0, color: Colors.grey)),
-              backgroundColor: Colors.white,
+              backgroundColor: Color(0xFFFAFAFA),
               elevation: 2.0, //设置阴影辐射范围
             ),
             preferredSize: Size.fromHeight(50)),
