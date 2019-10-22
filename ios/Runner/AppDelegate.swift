6af710e35fb1bbab9a1aeeeed9430ef87d9719a7
colin <287268221@qq.com>
2019-10-22 17:46:17 +0800

    update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,9 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+  var mResult: FlutterResult
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -20,14 +23,17 @@ import Flutter
                                               binaryMessenger: controller.binaryMessenger)
     payChannel.setMethodCallHandler({
       [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-      // Note: this method is invoked on the UI thread.
-      guard call.method == "paymentByPayPal" else {
+        // Note: this method is invoked on the UI thread.
+        guard call.method == "getBatteryLevel" else {
+          result(FlutterMethodNotImplemented)
+          return
+        }
+        mResult = result
+
         var money = call.argument("money")
         var currency = call.argument("currency")
         var packageName = call.argument("packageName")
         PayPalUtils.startPay(viewController: self, payPalPaymentDelegate: self, payAmount: money , currencyCode: currency, shortDescription: packageName);
-        return
-      }
 
     })
 
@@ -41,6 +47,11 @@ extension PlanCV: PayPalPaymentDelegate {
 		    func payPalPaymentDidCancel(_ paymentViewController: PayPalPaymentViewController) {
 		    	//运行到这里说明支付被取消
 		        dismiss(animated: false)
+		        if(mResult!=nil){
+		             mResult(FlutterError(code: "UNAVAILABLE",
+                                             message: "user cancel",
+                                             details: nil))
+		        }
 		    }
 
 		    func payPalPaymentViewController(_ paymentViewController: PayPalPaymentViewController
@@ -48,8 +59,12 @@ extension PlanCV: PayPalPaymentDelegate {
 		        do {
 		            let paymentId = try ((completedPayment.confirmation as NSDictionary).object(forKey: "response") as! [String: String])["id"]
 		            //可以将获取的paymentId直接上传至服务器处理，相关代码后面服务器部分已经说明
+		            if (mResult != null) {
+                          mResult.success(paymentId);
+                        }
 		        } catch {
 		        	//获取失败说明支付可能失败，做失败处理即可。
+		        	result(FlutterMethodNotImplemented)
 		        }
 		        dismiss(animated: false)
 		    }
